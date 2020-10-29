270a1f9a2f8de1e0f65714f0d16a30c31b147ee8
mattfan00 <kungfumo@gmail.com>
2020-10-29 00:03:31 -0400

    added in connection to db

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const db = process.env.MONGO_URI;
+
+mongoose.connect(db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log("connected to database"))
+  .catch(err => console.error(err))
+
 
 app.get("/test", (req, res) => {
   res.json({

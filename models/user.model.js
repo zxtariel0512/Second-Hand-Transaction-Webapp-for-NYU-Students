a8f95c31498c1e97848da57c9a34aced77f17c45
zxtariel0512 <66028314+zxtariel0512@users.noreply.github.com>
2020-11-01 21:46:47 -0500

    Update user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -1,10 +1,4 @@
 const mongoose = require('mongoose');
-// const Schema = mongoose.Schema;
-
-// const reviewSchema = new Schema({
-//     rating: {type: Number, required: true},
-//     description: {type: String, optional: true},
-// });
 
 var userSchema = new mongoose.Schema({
     netid: {type: String, require: true, unique: true, trim: true},
@@ -19,7 +13,6 @@ var userSchema = new mongoose.Schema({
     instagram: {type: String},
     facebook: {type: String},
     description: {type: String, maxlength: 30},
-    // reviews: {type: [reviewSchema], optional: true}
 });
 
 module.exports = mongoose.model('User', userSchema);

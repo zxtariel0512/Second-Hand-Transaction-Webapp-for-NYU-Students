8a24bd3984ea0b64da73ef7d75bf019fe8f2b677
Xintong Zhu <arielzhu@xintongs-mbp.myfiosgateway.com>
2020-11-06 22:03:21 -0500

    Add user routes

--- models/user.model.js
+++ models/user.model.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose');
 
 var userSchema = new mongoose.Schema({
     netid: {type: String, require: true, unique: true, trim: true},
+    username: {type: String, require: true, unique: true, trim: true},
     name: {type: String, required: true, unique: true},
     password: {type: String, required: true, unique: true},
     credit: {type: Number, required: true},
+    valid: {type: Boolean, required: true},
     schoolYear: {type: Number},
     major: {type: String},
     phone: {type: String},
@@ -12,7 +14,7 @@ var userSchema = new mongoose.Schema({
     venmo: {type: String},
     instagram: {type: String},
     facebook: {type: String},
-    description: {type: String, maxlength: 30},
+    descriptioBn: {type: String, maxlength: 30},
 });
 
 module.exports = mongoose.model('User', userSchema);

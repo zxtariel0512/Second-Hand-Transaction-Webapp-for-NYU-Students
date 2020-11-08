49e76b75d35d8c49e5c9698c7a5e80205d1180ce
Xintong Zhu <arielzhu@xintongs-mbp.myfiosgateway.com>
2020-11-08 13:52:41 -0500

    Add auth and /login route

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 let User = require('../models/user.model');
 
+const { auth } = require("../middleware/auth");
+
 router.route('/').get(async(req, res) => {
     let foundUsers = await User.find();
     res.json(foundUsers);
@@ -16,14 +18,24 @@ router.route('/register').post(async(req, res) => {
     res.json(newUser);
 })
 
-router.route('/:netid').put(async (req, res) => {
+router.route('/:netid').put(auth, async (req, res) => {
   let updatedUser = await User.findOneAndUpdate({netid: req.params.netid}, req.body, {new:true});
   res.json(updatedUser);
 })
 
-router.route('/:netid').delete(async (req, res) => {
+router.route('/:netid').delete(auth, async (req, res) => {
   let deletedUser = await User.findOneAndDelete({netid: req.params.netid});
   res.json(deletedUser);
 })
+
+router.route('/login/:netid').put(auth, async(req, res) =>{
+  let dumUser = await User.findOne({netid: req.params.netid});
+  dumUser.valid = true;
+  let validUser = await User.findOneAndUpdate({netid: req.params.netid}, dumUser, {new:true});
+  res.json(validUser);
+})
+
+
+
 
 module.exports = router;

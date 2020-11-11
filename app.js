b5d1a363e87c8a7ad2604c8e76606384db263903
mattfan00 <kungfumo@gmail.com>
2020-11-10 19:46:11 -0500

    socket server now works

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
 const express = require("express");
+const http = require("http")
 const cors = require("cors");
 const mongoose = require("mongoose");
-const socketio = require("socket.io");
 require("dotenv").config();
 
 const app = express();
-const server = http.createServer(app)
-const io = socketio(server)
+const server = http.createServer(app);
+//const io = require("socket.io")(server);
+const io = require("socket.io")(server, {
+    cors:true,
+});
+// const io = require("socket.io")(server, {
+//   handlePreflightRequest: (req, res) => {
+//       console.log("hi")
+//       const headers = {
+//           "Access-Control-Allow-Headers": "Content-Type, Authorization",
+//           "Access-Control-Allow-Origin": 'http://localhost:3000', //or the specific origin you want to give access to,
+//           "Access-Control-Allow-Credentials": true
+//       };
+//       res.writeHead(200, headers);
+//       res.end();
+//   }
+// })
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
 const db = process.env.MONGO_URI;
 console.log(db)
 mongoose.connect(db, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true
 })
-  .then(() => console.log("connected to database"))
-  .catch(err => console.error(err))
+    .then(() => console.log("connected to database"))
+    .catch(err => console.error(err))
 
 const userRouter = require('./routes/user');
 const listingRouter = require('./routes/listing');
@@ -32,6 +47,6 @@ app.use('/listings', listingRouter);
 app.use('/requests',requestRouter)
 ioRoutes
 
-app.listen(4000, () => {
-  console.log("secondhand server started");
+server.listen(4000, () => {
+    console.log("secondhand server started");
 })
--- client/src/View/Chat/index.js
+++ client/src/View/Chat/index.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
+const ENDPOINT = "http://localhost:4000";
 
 
 export default function Index(props) {
+    useEffect(() => {
+        const socket = io(ENDPOINT);
+        // fetch(ENDPOINT + '/listings', {
+        //     method: "GET"
+        // })
+        //     .then(res => res.json())
+        //     .then(data => {
+        //         console.log(data)
+        //     })
+    })
+
     return (
         <div>
             hello

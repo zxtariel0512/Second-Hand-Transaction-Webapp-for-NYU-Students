4d883f59e430375ed39057f879b68b28b2df539a
mattfan00 <kungfumo@gmail.com>
2020-11-10 22:07:09 -0500

    some communication, useState with arrays not working

--- client/src/View/Chat/index.js
+++ client/src/View/Chat/index.js
@@ -1,23 +1,46 @@
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
-const ENDPOINT = "http://localhost:4000";
 
+const ENDPOINT = "http://localhost:4000";
+let socket;
 
 export default function Index(props) {
+    const [response, setResponse] = useState("");
+    const [sendMessage, setSendMessage] = useState("");
+
     useEffect(() => {
-        const socket = io(ENDPOINT);
-        // fetch(ENDPOINT + '/listings', {
-        //     method: "GET"
-        // })
-        //     .then(res => res.json())
-        //     .then(data => {
-        //         console.log(data)
-        //     })
-    })
+        socket = io(ENDPOINT);
+        socket.on("welcome", data => {
+            console.log(data); //setResponse(data)
+        })
+        socket.on("newMessage", message => {
+            setAllMessages([...allMessages, message])
+            console.log(allMessages)
+        })
+    }, [])
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        setSendMessage("");
+        socket.emit("sendMessage", sendMessage);
+    }
 
     return (
         <div>
-            hello
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={sendMessage}
+                    onChange={e => setSendMessage(e.target.value)}
+                />
+                <input type="submit" value="Submit"/>
+            </form>
+
+            <div>
+                {allMessages.map(message => {
+                    <div>{message}</div>
+                })}
+            </div>
         </div>
     )
 }
--- routes/chat.js
+++ routes/chat.js
@@ -1,5 +1,14 @@
 module.exports = (io) => {
-  io.on("connection", (socket) => {
-    console.log("user connected")
-  })
+    io.on("connection", (socket) => {
+        console.log("user connected")
+        socket.emit("welcome", "Welcome to the room")
+
+        socket.on("sendMessage", (message) => {
+            io.emit("newMessage", message)
+        })
+
+        socket.on("disconnect", () => {
+            console.log("user disconnected")
+        })
+    })
 }

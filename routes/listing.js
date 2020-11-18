96854d650fbc1f801f78874e659937eaeca3b98c
mattfan00 <kungfumo@gmail.com>
2020-11-17 21:30:25 -0500

    added inline documentation for all the routes

--- routes/listing.js
+++ routes/listing.js
@@ -3,6 +3,7 @@ let Listing = require('../models/listing');
 
 const { auth } = require("../middleware/auth");
 
+// Get a list of all of the listings
 router.route('/').get(async (req, res) => {
     try {
         let foundListings = await Listing.find();
@@ -10,19 +11,21 @@ router.route('/').get(async (req, res) => {
     } catch (error) {
         res.status(500).json({message: "error: get all listing"})
     }
-    
+
 })
 
+// Get an individual listing
 router.route('/:id').get(async (req, res) => {
     try {
         let foundListing = await Listing.findById(req.params.id);
-        res.json(foundListing);   
+        res.json(foundListing);
     } catch (error) {
         res.status(500).json({message: "error: get specific listing"})
     }
-    
+
 })
 
+// Create a new listing
 router.route('/new').post(auth, async (req, res) => {
     try {
         let newListing = await Listing.create(req.body);
@@ -31,31 +34,34 @@ router.route('/new').post(auth, async (req, res) => {
         res.status(500).json({message: "error: create new listing"})
 
     }
-    
+
 })
 
+// Update individual listing
 router.route('/:id').put(auth, async (req, res) => {
     try {
         let updatedListing = await Listing.findByIdAndUpdate(req.params.id, req.body, {new:true});
         res.json(updatedListing);
-        
+
     } catch (error) {
         res.status(500).json({message: "error: update listing"})
     }
-    
+
 })
 
+// Delete individual listing
 router.route('/:id').delete(auth, async (req, res) => {
     try {
         let deletedListing = await Listing.findByIdAndDelete(req.params.id);
         res.json(deletedListing);
-        
+
     } catch (error) {
         res.status(500).json({message: "error: delete listing"})
     }
-    
+
 })
 
+// Find all listings associated with a users netid
 router.route('/netid/:netid').get(auth, async(req, res) => {
     let findListing = await Listing.find({user_id: req.params.netid});
     res.json(findListing);

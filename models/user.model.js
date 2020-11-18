4ffc48eb4a8b74034479160abefd56b5db60871c
Jason Yu <60864997+jy2575@users.noreply.github.com>
2020-11-18 13:30:25 -0800

    Update user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     netid: {type: String, require: true, unique: true, trim: true},
     username: {type: String, require: true, unique: true, trim: true},
     name: {type: String, required: true},
-    //deleted password field
+    password: {type: String, required: true},
     credit: {type: Number, required: true},
     valid: {type: Boolean, required: true},
     reviews: [{type: mongoose.Schema.Types.ObjectID, ref: 'Review'}],
